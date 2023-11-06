2a9db08b1389ed95a3039dc0f8dc0dc1c0ff57b3
Phil1436 <philxsb@gmail.com>
2023-11-06 10:00:30 +0100

    update

--- test/cmdtool_test.go
+++ test/cmdtool_test.go
@@ -31,45 +31,90 @@ func TestFlag_Copy(t *testing.T) {
 	}
 }
 
-func TestFlag_ToString(t *testing.T) {
+func TestFlagSet_AddFlag(t *testing.T) {
+	fs := &cmdtool.FlagSet{}
+	flag := &cmdtool.Flag{
+		Name:        []string{"flag1", "flag2"},
+		Description: "Test flag",
+		Value:       "test",
+		BoolFlag:    true,
+	}
+
+	fs.AddFlag(flag)
+
+	if !reflect.DeepEqual(fs.Flags[0], flag) {
+		t.Errorf("Expected flag to be added to cmdtool.FlagSet, but got %v", fs.Flags[0])
+	}
+}
+func TestFlagSet_GetValue(t *testing.T) {
+	fs := &cmdtool.FlagSet{}
+	flag1 := &cmdtool.Flag{
+		Name:        []string{"flag1"},
+		Description: "Test flag 1",
+		Value:       "test1",
+		BoolFlag:    true,
+	}
+	flag2 := &cmdtool.Flag{
+		Name:        []string{"flag2"},
+		Description: "Test flag 2",
+		Value:       "test2",
+		BoolFlag:    true,
+	}
+
+	fs.AddFlag(flag1)
+	fs.AddFlag(flag2)
+
 	tests := []struct {
-		name       string
-		flag       *cmdtool.Flag
-		wantResult string
+		name     string
+		flagName string
+		want     interface{}
 	}{
 		{
-			name: "single letter flag",
-			flag: &cmdtool.Flag{
-				Name:        []string{"f"},
-				Description: "Test flag",
-				Value:       nil,
-			},
-			wantResult: "   -f Test flag",
+			name:     "Get flag1 value",
+			flagName: "flag1",
+			want:     "test1",
 		},
 		{
-			name: "multi-letter flag",
-			flag: &cmdtool.Flag{
-				Name:        []string{"flag1", "flag2"},
-				Description: "Test flag",
-				Value:       nil,
-			},
-			wantResult: "   [-f|flag2] Test flag",
+			name:     "Get flag2 value",
+			flagName: "flag2",
+			want:     "test2",
 		},
 		{
-			name: "flag with value",
-			flag: &cmdtool.Flag{
-				Name:        []string{"f"},
-				Description: "Test flag",
-				Value:       "test",
-			},
-			wantResult: "   -f Test flag (optional)",
+			name:     "Get non-existent flag value",
+			flagName: "flag3",
+			want:     nil,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotResult := tt.flag.ToString(); gotResult != tt.wantResult {
-				t.Errorf("Flag.ToString() = %v, want %v", gotResult, tt.wantResult)
+			if got := fs.GetValue(tt.flagName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("cmdtool.FlagSet.GetValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+func TestFlagSet_IsDefined(t *testing.T) {
+	fs := &cmdtool.FlagSet{}
+	flag1 := &cmdtool.Flag{Name: []string{"flag1", "f1"}}
+	flag2 := &cmdtool.Flag{Name: []string{"flag2", "f2"}}
+	fs.AddFlag(flag1)
+	fs.AddFlag(flag2)
+
+	tests := []struct {
+		name     string
+		flagName string
+		want     bool
+	}{
+		{"defined flag", "flag1", true},
+		{"undefined flag", "flag3", false},
+		{"defined flag with alias", "f2", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := fs.IsDefined(tt.flagName); got != tt.want {
+				t.Errorf("cmdtool.FlagSet.IsDefined() = %v, want %v", got, tt.want)
 			}
 		})
 	}
